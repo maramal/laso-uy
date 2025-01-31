fb1a2e6d2ab53f638dc6815e4b3e3abb519b9de0
Martín Fernández <maramal@outlook.com>
2025-01-31 19:36:14 -0300

    agregado recaptcha a contacto

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,10 +1,13 @@
 import { Suspense } from "react";
 import ContactForm from "./contact-form";
+import { ReCaptchaProvider } from "next-recaptcha-v3"
 
 export default function ContactUsPage() {
     return (
         <Suspense>
-            <ContactForm />
+            <ReCaptchaProvider>
+                <ContactForm />
+            </ReCaptchaProvider>
         </Suspense>
     )
 }
